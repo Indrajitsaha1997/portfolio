c51ef3ab65baf9c71512a037af98010ef6d122d1
Indrajit7597 <indrasaha1997@gmail.com>
2021-02-26 16:06:53 +0530

    portfolio ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortfolioComponent } from '../app/portfolio/portfolio.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component:  PortfolioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
